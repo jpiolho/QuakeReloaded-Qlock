9b7d59ca6ab9ad9e1c8718018eeeb467b255fc3c
JPiolho <783467+jpiolho@users.noreply.github.com>
2022-11-07 22:38:42 +0100

    Initial commit

---
+++ Qlock/Mod.cs
@@ -0,0 +1,112 @@
+﻿using Qlock.Configuration;
+using Qlock.Template;
+using QuakeReloaded.Interfaces;
+using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using System.Globalization;
+
+namespace Qlock
+{
+    /// <summary>
+    /// Your mod logic goes here.
+    /// </summary>
+    public class Mod : ModBase // <= Do not Remove.
+    {
+        /// <summary>
+        /// Provides access to the mod loader API.
+        /// </summary>
+        private readonly IModLoader _modLoader;
+
+        /// <summary>
+        /// Provides access to the Reloaded.Hooks API.
+        /// </summary>
+        /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+        private readonly IReloadedHooks? _hooks;
+
+        /// <summary>
+        /// Provides access to the Reloaded logger.
+        /// </summary>
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Entry point into the mod, instance that created this class.
+        /// </summary>
+        private readonly IMod _owner;
+
+        /// <summary>
+        /// The configuration of the currently executing mod.
+        /// </summary>
+        private readonly IModConfig _modConfig;
+
+        private IntPtr _cvarClock;
+        private IntPtr _cvarClockX;
+        private IntPtr _cvarClockY;
+        private IntPtr _cvarClockAMPM;
+        private IntPtr _cvarClockGenericLocale;
+
+        public Mod(ModContext context)
+        {
+            _modLoader = context.ModLoader;
+            _hooks = context.Hooks;
+            _logger = context.Logger;
+            _owner = context.Owner;
+            _configuration = context.Configuration;
+            _modConfig = context.ModConfig;
+
+
+            if (!(_modLoader.GetController<IQuakeReloaded>()?.TryGetTarget(out var qreloaded) ?? false))
+                throw new Exception("Could not get QuakeReloaded API. Are you sure QuakeReloaded is installed & loaded before this mod?");
+
+            if (!(_modLoader.GetController<IQuakeUI>()?.TryGetTarget(out var ui) ?? false))
+                throw new Exception("Could not get QuakeReloaded UI API. Are you sure QuakeReloaded is installed & loaded before this mod?");
+
+            qreloaded.Events.RegisterOnPreInitialize(() =>
+            {
+                _cvarClock = qreloaded.Cvars.Register("scr_clock", "0", "Turn on or off a clock on screen. 0 = No clock, 1 = Game time (not implemented yet) 2 = 12h clock, 3 = 24h clock", CvarFlags.Float | CvarFlags.Saved, 0, 3);
+                _cvarClockX = qreloaded.Cvars.Register("scr_clock_x", "0.90", "X position of the clock (0: Left edge, 1: Right edge)", CvarFlags.Float | CvarFlags.Saved, 0, 1);
+                _cvarClockY = qreloaded.Cvars.Register("scr_clock_y", "0.10", "Y position of the clock (0: Top edge, 1: Bottom edge)", CvarFlags.Float | CvarFlags.Saved, 0, 1);
+                _cvarClockAMPM = qreloaded.Cvars.Register("scr_clock_ampm", "1", "In 12h format, show AM/PM", CvarFlags.Boolean | CvarFlags.Saved);
+                _cvarClockGenericLocale = qreloaded.Cvars.Register("scr_clock_genericlocale", "0", "If set to 1, it'll use generic language instead of system language settings", CvarFlags.Boolean | CvarFlags.Saved);
+            });
+
+
+            qreloaded.Events.RegisterOnRenderFrame(() =>
+            {
+                var clock = qreloaded.Cvars.GetFloatValue(_cvarClock);
+                if (clock > 0)
+                {
+                    var dt = DateTime.Now;
+                    var locale = qreloaded.Cvars.GetBoolValue(_cvarClockGenericLocale,false) ? CultureInfo.InvariantCulture : CultureInfo.CurrentCulture;
+                    if (clock == 2)
+                    {
+                        var ampm = qreloaded.Cvars.GetBoolValue(_cvarClockAMPM);
+                        ui.DrawText(dt.ToString(ampm ? "hh:mmtt" : "hh:mm", locale), qreloaded.Cvars.GetFloatValue(_cvarClockX, 0.90f), qreloaded.Cvars.GetFloatValue(_cvarClockY, 0.10f));
+                    }
+                    else if (clock == 3)
+                    {
+                        ui.DrawText(dt.ToString("HH:mm", locale), qreloaded.Cvars.GetFloatValue(_cvarClockX, 0.90f), qreloaded.Cvars.GetFloatValue(_cvarClockY, 0.10f));
+                    }
+                }
+            });
+
+
+
+        }
+
+        #region Standard Overrides
+        public override void ConfigurationUpdated(Config configuration)
+        {
+            // Apply settings from configuration.
+            // ... your code here.
+            _configuration = configuration;
+            _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+        }
+        #endregion
+
+        #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Mod() { }
+#pragma warning restore CS8618
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Qlock
+<p align="center">An engine mod for Quake Enhanced that an in-game clock to the UI</p>
+<p align="center">This mod uses <a href="https://github.com/Reloaded-Project/Reloaded-II">Reloaded-II</a> and <a href="https://github.com/jpiolho/QuakeReloaded">QuakeReloaded</a></p>
+<p align="center"><img width="256" height="256" alt="Logo" src="https://github.com/jpiolho/QuakeReloaded-Qlock/blob/main/Qlock/Preview.png"></p>
+
+
+# How to configure
+With this mod, the following cvars are available:
+* `scr_clock` - 0: No clock, 1: In-game time (Not implemented yet), 2: 12h real world clock, 3: 24h real world clock
+* `scr_clock_x` - Customize the X position of the clock
+* `scr_clock_y` - Customize the Y position of the clock`
+* `scr_clock_ampm` - In 12h format, show AM/PM
+* `scr_clock_genericlocale` - If set to 1, it'll use a generic language format instead of the system language
+
+# How to install
+1. Download and install [Reloaded-II](https://github.com/Reloaded-Project/Reloaded-II) if you don't have it
+2. Install [QuakeReloaded](https://github.com/jpiolho/QuakeReloaded) mod if you don't have it
+3. Download the latest version of Qlock at the [releases](https://github.com/jpiolho/QuakeReloaded-Qlock/releases) page
